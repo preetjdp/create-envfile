ad20c279bb5bd566b10eafa9a77a537b3b1aa6f1
preetjdp <preetjdp123@gmail.com>
2020-10-31 12:32:31 +0530

    Spit out all environment variables

--- src/create-envfile.py
+++ src/create-envfile.py
@@ -5,8 +5,9 @@ env_keys = list(dict(os.environ).keys())
 out_file = ""
 
 for key in env_keys:
-    if key.startswith("INPUT_ENVKEY_"):
-        out_file += key.split("INPUT_ENVKEY_")[1] + "=" + os.environ.get(key) + "\n"
+    # if key.startswith("INPUT_ENVKEY_"):
+    #     out_file += key.split("INPUT_ENVKEY_")[1] + "=" + os.environ.get(key) + "\n"
+    out_file += key + "=" + os.environ.get(key) + "\n"
 
 with open("/github/workspace/" + str(os.environ.get("INPUT_FILE_NAME")), "w") as text_file:
     text_file.write(out_file)
