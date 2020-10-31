49f16c338febd0a8c6f56b0d99ccbcfa6fd8365c
preetjdp <preetjdp123@gmail.com>
2020-10-31 12:45:47 +0530

    more test

--- src/create-envfile.py
+++ src/create-envfile.py
@@ -1,10 +1,13 @@
 import os
 
+print('wowza')
+
 env_keys = list(dict(os.environ).keys())
 
 out_file = ""
 
 for key in env_keys:
+    print('yes',key)
     # if key.startswith("INPUT_ENVKEY_"):
     #     out_file += key.split("INPUT_ENVKEY_")[1] + "=" + os.environ.get(key) + "\n"
     out_file += key + "=" + os.environ.get(key) + "\n"
